1b4262b4283bb99cbc1558cae77833d5226213ce
ahsmha <ahmed.5wna0@aleeas.com>
2023-05-14 12:41:44 +0530

    add post router

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +15,23 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipeHandler)
 	router.Run()
 }
